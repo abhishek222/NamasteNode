456200ef4bd91ee19330c100c70a080ac314803f
Tejashri1128 <tejudatir@gmail.com>
2025-02-03 20:05:43 +0530

    created server and crud operations using mongoDB

--- database.js
+++ database.js
@@ -1,7 +1,45 @@
-require("mongodb");
+const { MongoClient } = require("mongodb");
 
-const URI =
+const url =
   "mongodb+srv://abhishekgadhave222:IWDCvaE7IG0BfZVj@namastenode.llf0l.mongodb.net/";
+
+const client = new MongoClient(url);
+// Database Name
+const dbName = "HelloWorld";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("User");
+
+  // Insert some documents
+
+  const data = {
+    firstName: "John",
+    lastName: "Doe",
+    phoneNumber: "123",
+    city: "New York",
+  };
+  //   const insertResult = await collection.insertMany([data]);
+  //   console.log("Inserted documents =>", insertResult);
+  //Read
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  const countResult = await collection.countDocuments({}, true);
+  console.log("Total documents =>", countResult);
+
+  const result = await collection.find({ firstName: "John" }).toArray();
+  console.log("result", result);
+
+  return "done.";
+}
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 
 //notes
 // go to mongoDB
@@ -10,3 +48,5 @@ const URI =
 // Get the connection string
 // install mongodb compass
 // create connection using below connection string
+// create a database
+// crud operations
